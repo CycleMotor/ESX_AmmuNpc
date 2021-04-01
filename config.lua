faf0b7f0483a74ded7bed15ab82f2c4a76bb75b5
Cycle <81378599+CycleMotor@users.noreply.github.com>
2021-04-01 19:31:30 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config.ArgentSale = false  --true if use of black money, false for regular money
+
+Config.Weapons = {
+    {weapon = "WEAPON_CROWBAR", price = 115},
+    {weapon = "WEAPON_DAGGER", price = 200},
+    {weapon = "WEAPON_MOLOTOV", price = 2750},    
+    {weapon = "WEAPON_PISTOL50", price = 12800},
+    {weapon = "WEAPON_DOUBLEACTION", price = 16200},
+    {weapon = "WEAPON_SAWNOFFSHOTGUN", price = 22400},
+    {weapon = "WEAPON_MINISMG", price = 24800},
+    {weapon = "WEAPON_GUSENBERG", price = 42500}, 
+}
+
+--Weapon List : https://wiki.rage.mp/index.php?title=Weapons
+
+----------------------------------------
+--Dev by Cycle--
+----------------------------------------
